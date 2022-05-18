31bff1f6e4edf4288e59168f0c02c0c39bb53d93
Kevin Nielsen <knielsen24@gmail.com>
2022-05-17 22:11:02 -0700

    commit tree 577915e38c84dc72b695ac7e7704a00a5fe612e5

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+  return (
+	 <div>
+		 <button onBlur={(e) => console.log('Hey! Eyes on me!')} onFocus={(e) => console.log('Good!')}>Eyes on me</button>
+	 </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+  
+	function change(e) {
+		console.log('Entering password...')
+	}
+	return (
+		<div>
+			<input type="password" onChange={change} placeholder="Password" ></input>
+		</div>
+	)
+}
+
+export default Keypad
